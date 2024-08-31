6a77db569ef252655ab05086d415d10ac6ee4f7a
mdrabil <ermdrabil@gmail.com>
2024-08-31 11:10:01 +0530

    niche

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ app.use(cors(
 app.use(bodyParser.json())
 
 mongoose.connect(process.env.URL).then(()=>{
-  console.log("Database is connect")
+  console.log("Database connect")
 }).catch(()=>{
   console.log("DB not connect")
 })
+
 
 
 
